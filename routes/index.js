26009d4380a4ce1a103278c4645c73fcb8c1ec0b
TheMax2 <max.malyshev.282@gmail.com>
2020-09-15 15:02:24 -0700

    added userSchema to database

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,94 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/book');
 
-router.get('/', async (req, res) => {
-    let books
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const User = require('../models/user');
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+let users = [];
+
+router.use(express.urlencoded({ extended: false}))
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+router.use(passport.initialize())
+router.use(passport.session())
+router.use(methodOverride('_method'))
+
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name})
+})
+
+router.get('/login', checkNotAuthenticated, async (req, res) => {
+    users = await User.find();
+    res.render('login.ejs')
+})
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+router.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs', {name: ''})
+})
+router.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        books = await Book.find().sort({creatAt: 'desc'}).limit(10).exec();
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.forEach(u => {
+            if (u.email === req.body.email){
+                console.log("ALREADY USER WITH THAT EMAIL");
+                //res.redirect('/register');
+                const params = {
+                    error: "Error: already a user with that email"
+                }
+                res.render('register.ejs', params);
+                return;
+                //return;
+            }
+        });
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        await user.save();
+        res.redirect('/login')
     } catch {
-        books = [];
+        res.redirect('/register')
     }
-    res.render('index', {books: books});
-});
+    console.log(users)
+})
+
+router.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 module.exports = router;
